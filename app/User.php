7e3ba10713e8d3974f1b7e51ab1bea466545d7e4
Islam Dudaev <islamwolf23@gmail.com>
2018-01-07 02:08:17 +0000

    Role relationship setup
    Setting up relationship between users and roles tables and created Admin controller

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\App;
 
 class User extends Authenticatable
 {
@@ -26,4 +27,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    public function role()
+    {
+        return $this->belongsTo('App\Role');
+    }
+
 }
